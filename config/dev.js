d9f84f4d37f05a005c6d1242e2d149d9a518154c
avishai etach <etach89@gmail.com>
2021-10-17 16:44:24 +0300

    sadsad

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  //'dbURL': 'mongodb://localhost:27017',
-  'dbURL':'mongodb+srv://omer_morag:omer2003@cluster0.tyezt.mongodb.net/myFirstDataBase?retryWrites=true&w=majority'
-}
+  dbURL: "mongodb://localhost:27017",
+  // 'dbURL':'mongodb+srv://omer_morag:omer2003@cluster0.tyezt.mongodb.net/myFirstDataBase?retryWrites=true&w=majority'
+};
