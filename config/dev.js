54bebb50ad57b288440522e163f01475d1ef867a
ShaulBatzon <shaulbatzon1@gmail.com>
2022-01-16 17:50:31 +0200

    new db connection

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: "mongodb://localhost:27017",
-  // dbURL: '//cinco:uVLPnqvvhpdWvhEC@cinco.4t8q1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  // dbURL: "mongodb://localhost:27017",
+  dbURL: '//cinco:uVLPnqvvhpdWvhEC@cinco.4t8q1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 };
